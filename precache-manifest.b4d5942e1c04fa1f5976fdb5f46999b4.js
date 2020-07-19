a5c1c0c5a3fc0485b75755de856e88148fd2066e
Reybert-APS <rapenuela5@misena.edu.co>
2020-07-19 10:59:10 -0500

    Updates

---
+++ precache-manifest.b4d5942e1c04fa1f5976fdb5f46999b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b38edb57322c16cfd5a4cb8d28bead4",
+    "url": "/ReactApp.github.io/index.html"
+  },
+  {
+    "revision": "68ea3fbf797cf3360a19",
+    "url": "/ReactApp.github.io/static/css/main.b6633d33.chunk.css"
+  },
+  {
+    "revision": "7c4a6af3afa948952131",
+    "url": "/ReactApp.github.io/static/js/2.95d37d32.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/ReactApp.github.io/static/js/2.95d37d32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68ea3fbf797cf3360a19",
+    "url": "/ReactApp.github.io/static/js/main.4736e4fe.chunk.js"
+  },
+  {
+    "revision": "e8def105cf8731cf61cf",
+    "url": "/ReactApp.github.io/static/js/runtime-main.1a545f1d.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ReactApp.github.io/precache-manifest.b4d5942e1c04fa1f5976fdb5f46999b4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactApp.github.io/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
